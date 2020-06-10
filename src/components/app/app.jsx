b01da2cebb561b6f9fb657729434551229d73a1e
Alexey <89641234561@mail.ru>
2020-06-10 17:43:21 +0800

    Создает Пропсы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Main from "../main/main.jsx";
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {filmName, filmGenre, filmDate} = props;
   return (
-    <Main/>
+    <Main
+      filmName={filmName}
+      filmGenre={filmGenre}
+      filmDate={filmDate}
+    />
   );
 };
 
