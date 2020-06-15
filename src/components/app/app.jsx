84fac20eda0ca296e8435b177617fcb4444381a6
Alexey <89641234561@mail.ru>
2020-06-15 21:12:24 +0800

    Убирает строку Eslint

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,6 @@ import Main from "../main/main.jsx";
 import PropTypes from 'prop-types';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {filmName, filmGenre, filmDate, films} = props;
   return (
     <Main
